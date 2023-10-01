71aa17b10782843180394fc82a2873e5bee467a0
Vlad-Mutruc <v.mutruc@student.utwente.nl>
2023-10-01 16:59:22 +0200

    Setup copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,21 @@
 return {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
+  init = function()
+    require("copilot").setup({
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = "<M-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+    })
+  end,
 }
